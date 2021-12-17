04663de7a65062f666d1eaeaec25ee593b1ad2d8
brjohnson0525 <70598479+brjohnson0525@users.noreply.github.com>
2021-12-16 17:23:50 -0800

    Delete BriannaJohnsonCapstone/BriannaJohnsonCapstone directory

--- BriannaJohnsonCapstone/BriannaJohnsonCapstone/src/main/java/com/company/BriannaJohnsonCapstone/gameDao/dao/ESRBRatingDao.java
+++
@@ -1,9 +0,0 @@
-package com.company.BriannaJohnsonCapstone.gameDao.dao;
-
-public interface ESRBRatingDao {
-    ESRBRating addESRBRating(ESRBRating esrbRating);
-    ESRBRating getESRBRating(int id);
-    List<ESRBRating> getAllESRBRatings();
-    void updateESRBRating(ESRBRating esrbRating);
-    void deleteESRBRating(int id);
-}
--- BriannaJohnsonCapstone/BriannaJohnsonCapstone/src/main/java/com/company/BriannaJohnsonCapstone/gameDao/dao/StudioDao.java
+++
@@ -1,9 +0,0 @@
-package com.company.BriannaJohnsonCapstone.gameDao.dao;
-
-public interface StudioDao {
-    Studio addStudio(Studio studio);
-    Studio getStudio(int id);
-    List<Studio> getAllStudios();
-    void updateStudio(Studio studio);
-    void deleteStudio(int id);
-}
